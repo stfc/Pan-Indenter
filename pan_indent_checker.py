67dc2545a2e913ec7788307caada14e13874111a
Russell Seers <devdeve@minion.esc.rl.ac.uk>
2018-06-25 10:26:14 +0100

    File name change and comment remover from main code. also adding license to code

--- pan_indent_checker.py
+++ pan_indent_checker.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python2
 
+# Copyright 2018 Science & Technology Facilities Council
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import argparse
 import re
 
@@ -8,11 +22,6 @@ DEBUG_LINE = "\033[36m%-16s \033[35m|\033[0m %s"
 
 # Indent space to use to format the code
 INDENT = '    '
-
-
-# def _print_debug(k, v):
-#     print DEBUG_LINE % (k, v)
-#     pass
 
 def main():
 
